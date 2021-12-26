e96b968158829212f7670b976f6fc61687376a4a
c3eru <96016081+c3eru@users.noreply.github.com>
2021-12-26 07:28:14 +0700

    Update pe

--- final.sh
+++ final.sh
@@ -7,6 +7,10 @@ curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
+export ALLOW_MISSING_DEPENDENCIES=true
+export TZ=Asia/Jakarta
+export BUILD_USERNAME=mobxprjkt
+export BUILD_HOSTNAME=c3eru
 ccache -M 20G
 ccache -o compression=true
 ccache -z
