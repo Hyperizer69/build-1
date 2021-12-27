14c95c4faff08f1bba577a2e3e18c0312b22508e
c3eru <96016081+c3eru@users.noreply.github.com>
2021-12-27 22:21:25 +0700

    Update final.sh

--- final.sh
+++ final.sh
@@ -2,7 +2,7 @@
 cd /tmp/rom
 
 . build/envsetup.sh
-lunch aosp_juice-userdebug
+lunch dot_juice-userdebug
 curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="$(echo "${var_cache_report_config}")"
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
@@ -10,4 +10,4 @@ export USE_CCACHE=1
 ccache -M 20G
 ccache -o compression=true
 ccache -z
-mka bacon -j8
+make bacon -j8
