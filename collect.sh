083698ed46c3e987505af6949052f7254d296473
mobxCode <yovie079@gmail.com>
2022-01-07 09:11:43 +0700

    bringup CAF-EX

--- collect.sh
+++ collect.sh
@@ -2,17 +2,19 @@
 cd /tmp/rom
 
 . build/envsetup.sh
-lunch aosp_juice-userdebug
+lunch cafex_juice-userdebug
 curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="$(echo "${var_cache_report_config}")"
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
 export ALLOW_MISSING_DEPENDENCIES=true
 export SELINUX_IGNORE_NEVERALLOW=true
+export BUILD_USERNAME=mobxprjkt
+export BUILD_HOSTNAME=yoviie
 ccache -M 20G
 ccache -o compression=true
 ccache -z
-mka bacon -j8 &
+m cafex -j8 &
 sleep 90m
 kill %1
 ccache -s
