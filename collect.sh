f39715891a425b3b8e1299080a2594ccf6970156
c3eru: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 <yyprjkt@gmail.com>
2022-01-10 14:29:33 +0700

    Initial Commit

--- collect.sh
+++ collect.sh
@@ -3,18 +3,15 @@ cd /tmp/rom
 
 . build/envsetup.sh
 lunch conquer_juice-userdebug
-curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="$(echo "${var_cache_report_config}")"
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
 export ALLOW_MISSING_DEPENDENCIES=true
 export SELINUX_IGNORE_NEVERALLOW=true
-export BUILD_USERNAME=mobxprjkt
-export BUILD_HOSTNAME=yoviie
-ccache -M 20G
+ccache -M 50G
 ccache -o compression=true
 ccache -z
 make carthage -j8 &
-sleep 90m
+sleep 50m
 kill %1
 ccache -s
