f36190188174a1c21d1d7ba706b3f9cf6908d443
c3eru <yyprjkt@gmail.com>
2022-01-14 18:26:55 +0700

    test

--- collect.sh
+++ collect.sh
@@ -3,16 +3,19 @@ cd /tmp/rom
 
 . build/envsetup.sh
 lunch aosp_juice-userdebug
-curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="$(echo "${var_cache_report_config}")"
+
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
 export ALLOW_MISSING_DEPENDENCIES=true
 export SELINUX_IGNORE_NEVERALLOW=true
+
 ccache -M 20G
 ccache -o compression=true
 ccache -z
-mka bacon -j8 &
+
+m rohie -j8 &
+
 sleep 90m
 kill %1
 ccache -s
